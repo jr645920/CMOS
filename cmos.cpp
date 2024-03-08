4ecb3e6e80824c7c8b40da87afc17c153969ccc7
Johnathan Reilly <jr645920@ohio.edu>
2024-03-08 12:43:30 -0500

    adding file descriptions

--- cmos.cpp
+++ cmos.cpp
@@ -1,5 +1,11 @@
+//*****************************************************************************************
+// Filename: cmos.cpp
+// Authors: Hunter Smith and Johnathan Reilly
+// Brief: A c++ program that uses the Winnowing algorithm to detect plagarism in
+//        pre-tokenized student sumbission files.
+// ****************************************************************************************
+
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <vector>
 #include <string>
@@ -7,6 +13,7 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
@@ -37,6 +44,7 @@ vector<vector<string>> read_in_tokens() {
     return tokens;
 }
 
+
 // generate k-grams from a sequence of tokens from read_in_tokens
 vector<string> generate_kgrams(const vector<string>& tokens, int k) {
     vector<string> kgrams;
@@ -60,12 +68,12 @@ size_t hash_string(const string& str) {
 }
 
 // perform the Winnowing algorithm
-vector<size_t> winnow(const vector<string>& tokens, int k, int w) {
+vector<size_t> winnow(const vector<string>& tokens, int k, int window_size) {
     vector<string> kgrams = generate_kgrams(tokens, k);
     vector<size_t> fingerprints;
 
-    for (size_t i = 0; i <= kgrams.size() - w; i++) {
-        string window_min = *min_element(kgrams.begin() + i, kgrams.begin() + i + w); // global position is just mininmum element
+    for (size_t i = 0; i <= kgrams.size() - window_size; i++) {
+        string window_min = *min_element(kgrams.begin() + i, kgrams.begin() + i + window_size); // global position is just mininmum element
         fingerprints.push_back(hash_string(window_min));
     }
 
@@ -74,25 +82,23 @@ vector<size_t> winnow(const vector<string>& tokens, int k, int w) {
 
 // calculate similarity (int didn't work so size_t had to be used)
 double calculate_similarity(const unordered_set<size_t>& set_one, const unordered_set<size_t>& set_two) {
-    unordered_set<size_t> intersection; // use set/map for kgrams
+    unordered_set<size_t> intersection;
     for (size_t hash_val : set_one) {
-
         if (set_two.find(hash_val) != set_two.end()) {
             intersection.insert(hash_val);
         }
-
     }  
 
-    double in_common_size = intersection.size();
-    double total_size = set_one.size() + set_two.size() - in_common_size;
-
-    return in_common_size / total_size; 
+    // can't use int I guess? size_t works
+    size_t intersection_size = intersection.size();
+    size_t union_size = set_one.size() + set_two.size() - intersection_size;
+    return static_cast<double>(intersection_size) / union_size;
 }
 
 int main() {
     // Experiment with k-gram and window size
-    int k = 10;            
-    int w = k; 
+    int k = 5;            
+    int window_size = 10; 
 
     // Read in tokens
     vector<vector<string>> submissions = read_in_tokens();
@@ -102,11 +108,11 @@ int main() {
 
     for (const auto& submission : submissions) {
         string submission_id = submission[0];
-        vector<size_t> fingerprints = winnow(submission, k, w);
+        vector<size_t> fingerprints = winnow(submission, k, window_size);
         submission_fingerprints[submission_id] = fingerprints;
     }
 
-    // calculate similarity 
+    //calculate similarity 
     unordered_set<string> processed_pairs; // need to keep track of processed pairs
 
     for (auto first_pair = submission_fingerprints.begin(); first_pair != submission_fingerprints.end(); ++first_pair) {
@@ -118,10 +124,9 @@ int main() {
             const string& id_two = second_pair -> first;
 
             // check if pair is processed or IDs are the same, only need to print once
-            if (processed_pairs.count(id_one + id_two) || processed_pairs.count(id_two + id_one) || id_one == id_two){
+            if (processed_pairs.count(id_one + id_two) || processed_pairs.count(id_two + id_one) || id_one == id_two)
                 continue;
-            }
-               
+
             const vector<size_t>& finerprints_two = second_pair -> second;
             double similarity = calculate_similarity(unordered_set<size_t>(finerprints_one.begin(), finerprints_one.end()), unordered_set<size_t>(finerprints_two.begin(), finerprints_two.end()));
             cout << "Similarity between " << id_one << " and " << id_two << ": " << similarity * 100 << setprecision(4) << "%" << endl; // Set precision to two decimal places
